1befe56727c0bf4c69d44234a8fb168648662998
Vít Ondruch <vondruch@redhat.com>
2021-12-07 17:31:35 +0100

    Add make to the buildroot.
    https://fedoraproject.org/wiki/Changes/Remove_make_from_BuildRoot

--- ruby-devel-srpm.rb
+++ ruby-devel-srpm.rb
@@ -50,7 +50,7 @@ end
 
 mock = Mock.new :root => 'ruby'
 
-mock.install %w(autoconf bison ruby rubypick rubygems rubygem-json git)
+mock.install %w(make autoconf bison ruby rubypick rubygems rubygem-json git)
 
 GITURL = URI.parse("https://github.com/ruby/ruby")
 mock.chroot "git clone #{GITURL} ~/ruby"
